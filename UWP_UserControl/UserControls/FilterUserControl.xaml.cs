d8dc90d958de398ce97decaa967040cc6a214799
David Ishaku <gibiuya@live.com>
2022-02-23 10:41:54 +0100

    Text edit

--- UWP_UserControl/UserControls/FilterUserControl.xaml.cs
+++ UWP_UserControl/UserControls/FilterUserControl.xaml.cs
@@ -30,7 +30,7 @@ namespace UWP_UserControl.UserControls
             get { return (string)GetValue(SearchProductIDProperty); }
             set { SetValue(SearchProductIDProperty, value); }
         }
-        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        // Using a DependencyProperty as the backing store for SearchProductID.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty SearchProductIDProperty =
             DependencyProperty.Register(nameof(SearchProductID), typeof(string), typeof(FilterUserControl), new PropertyMetadata(null));
 
@@ -39,7 +39,7 @@ namespace UWP_UserControl.UserControls
             get { return (string)GetValue(SearchProductNameProperty); }
             set { SetValue(SearchProductNameProperty, value); }
         }
-        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        // Using a DependencyProperty as the backing store for SearchProductName.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty SearchProductNameProperty =
             DependencyProperty.Register(nameof(SearchProductName), typeof(string), typeof(FilterUserControl), new PropertyMetadata(null));
 
@@ -48,7 +48,7 @@ namespace UWP_UserControl.UserControls
             get { return (List<string>)GetValue(SearchDiscontinuedListProperty); }
             set { SetValue(SearchDiscontinuedListProperty, value); }
         }
-        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        // Using a DependencyProperty as the backing store for SearchDiscontinuedList.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty SearchDiscontinuedListProperty =
             DependencyProperty.Register(nameof(SearchDiscontinuedList), typeof(List<string>), typeof(FilterUserControl), new PropertyMetadata(null));
 
@@ -57,7 +57,7 @@ namespace UWP_UserControl.UserControls
             get { return (string)GetValue(SearchProductDiscontinuedProperty); }
             set { SetValue(SearchProductDiscontinuedProperty, value); }
         }
-        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        // Using a DependencyProperty as the backing store for SearchProductDiscontinued.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty SearchProductDiscontinuedProperty =
             DependencyProperty.Register(nameof(SearchProductDiscontinued), typeof(string), typeof(FilterUserControl), new PropertyMetadata(null));
 
@@ -66,7 +66,7 @@ namespace UWP_UserControl.UserControls
             get { return (ICommand)GetValue(ClearCommandProperty); }
             set { SetValue(ClearCommandProperty, value); }
         }
-        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        // Using a DependencyProperty as the backing store for ClearCommand.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty ClearCommandProperty =
             DependencyProperty.Register(nameof(ClearCommand), typeof(ICommand), typeof(FilterUserControl), new PropertyMetadata(null));
     }
